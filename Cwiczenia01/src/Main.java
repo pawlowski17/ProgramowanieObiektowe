d987f9e89630dfc2458d86c322d9d97cac367040
Paweł Kocjan <pawe@gmail.com>
2026-09-11 09:43:20 +0200

    Instrukcje wypisana

--- Cwiczenia01/src/Main.java
+++ Cwiczenia01/src/Main.java
@@ -1,6 +1,6 @@
 import java.sql.SQLOutput;
 
-void main(){
+void main() {
     System.out.println("Hello World"); // Wypisz i zrób enter
     System.out.println("sout"); // sout
 
@@ -14,4 +14,24 @@ void main(){
     System.out.println("\"cudzysłów\"");
     System.out.println("\'apostrof\'");
     System.out.println("backslash: \\");
+
+    int liczbaCalkowita = 5;
+    double liczbaRzeczywista;
+
+    liczbaRzeczywista = (double) liczbaCalkowita /2;
+    System.out.println("Wynik dzielenia liczby 5 przez 2 to " + liczbaRzeczywista);
+
+    char znak = 'a';
+    System.out.println("Litera a ma wartość " + (znak + 0));
+
+    String napis = "napis";
+    System.out.println("Napis wielkimi literami: " + napis.toUpperCase());
+
+    System.out.println("Wynik 1 + 2 + 3 = " + 1 + 2 + 3);
+    System.out.println("Wynik 1 + 2 + 3 = " +( 1 + 2 + 3));
+    System.out.println(1 + 2 + 3 + "to wynik 1 + 2 + 3");
+
+
 }
+
+
