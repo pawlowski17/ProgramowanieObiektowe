7569adc5c078c7f5315b6d9d2bfba6058dfce33f
Paweł Kocjan <Pawel@gmail.com>
2026-09-24 11:32:07 +0200

    zad2

--- Zadania2/src/Main.java
+++ Zadania2/src/Main.java
@@ -1,15 +1,49 @@
 void main() {
     Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Podaj liczbę: ");
-            int liczba = scanner.nextInt();
+    System.out.print("Podaj numer miesiąca (1-12): ");
+    int numer = scanner.nextInt();
 
-            if (liczba % 3 == 0) {
-                System.out.println("Liczba " + liczba + " jest podzielna przez 3.");
-            } else {
-                System.out.println("Liczba " + liczba + " nie jest podzielna przez 3.");
-
-
+    switch (numer) {
+        case 1:
+            System.out.println("Styczeń");
+            break;
+        case 2:
+            System.out.println("Luty");
+            break;
+        case 3:
+            System.out.println("Marzec");
+            break;
+        case 4:
+            System.out.println("Kwiecień");
+            break;
+        case 5:
+            System.out.println("Maj");
+            break;
+        case 6:
+            System.out.println("Czerwiec");
+            break;
+        case 7:
+            System.out.println("Lipiec");
+            break;
+        case 8:
+            System.out.println("Sierpień");
+            break;
+        case 9:
+            System.out.println("Wrzesień");
+            break;
+        case 10:
+            System.out.println("Październik");
+            break;
+        case 11:
+            System.out.println("Listopad");
+            break;
+        case 12:
+            System.out.println("Grudzień");
+            break;
+        default:
+            System.out.println("Nieprawidlowy numer miesiaca");
+            break;
     }
 
 }
